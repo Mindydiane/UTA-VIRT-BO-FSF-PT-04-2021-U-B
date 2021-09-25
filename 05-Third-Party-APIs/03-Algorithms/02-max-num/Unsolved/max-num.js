1e5d54cf354e95a1faa3e3ae466ed9005580c9f6
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-24 20:44:54 -0500

    max-num algorithms

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,9 @@
 // Write code to return the largest number in the given array
+ var arr = [3, 1, 17, 5, 6];
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    
+   return Math.max.apply(null, arr);
+   
+};
+
