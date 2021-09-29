fcf4bc5a7d3f752ef8f75063c202a22448b95661
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-28 22:16:14 -0500

    log-even-nums algorithm

--- 04-Web-APIs/03-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
+++ 04-Web-APIs/03-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,15 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function(num) {
+
+    for(let i = 0; i <= num; i++) {
+        if(i % 2=== 0) {
+            console.log(i);
+        }
+    }
+    // console.log( 0== '0');
+    // console.log( 0=== '0');
+    // console.log(typeof 0)
+    // console.log(typeof '0')
+};
