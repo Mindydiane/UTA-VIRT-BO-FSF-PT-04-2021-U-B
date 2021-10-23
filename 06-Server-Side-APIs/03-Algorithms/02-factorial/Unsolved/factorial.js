e355dbada976f03eed92dfac3691be0d09fad6e7
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-23 14:07:00 -0500

    algorithm factorial practice

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,25 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    var result = 1;
+    
+    for (var i = num; i > 1; i--) {
+        result = result *i;
+    }
+    console.log(`
+    ===================
+    This is the result:
+    ===================
+    `);
+    console.log(result);
+
+    console.log(`
+    ===================
+    This is the number:
+    ===================
+    `);
+    console.log(num);
+    return result;
+
+
+};
