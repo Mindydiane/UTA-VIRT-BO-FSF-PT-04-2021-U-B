6649f6a025f3eb866352cedab1fee8b006299d3b
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-29 11:56:02 -0500

    isPalindrome algorithm passed

--- 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,14 +1,10 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
 var isPalindrome = function(str) {
-    var reversedString = str
-        .split("")
-        .reversed()
-        .join("")
-    
-    if (reversedString === str) {
-        return true;
-    } else {
-        return false;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length -1 -i]) {
+            return false
+        }
     }
+    return true;
 };
