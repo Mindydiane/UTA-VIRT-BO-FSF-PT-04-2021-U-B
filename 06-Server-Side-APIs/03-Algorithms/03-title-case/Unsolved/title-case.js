bee409259bfcabb65112d2e3698740b2e4e66de2
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-02 18:06:06 -0500

    practice title case algorithms

--- 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
+++ 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,18 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    var result = [];
+
+    var letters = str.split(" ");
+
+    for (var i = 0; i < letters.length; i++) {
+        var letter = letters[i].split("");
+
+        letter[0] = letter[0].toUpperCase();
+
+        result.push(letter.join(""));
+    }
+
+    return result.join(" ");
+
+};
