5e8837e68c52ade817c18f95549b4e40f4e14e02
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-04 23:17:32 -0500

    react app update

---
+++ 20-React/20-React/01-Activities/00-React-App/build/precache-manifest.4dd3f6f6532728eb5541c751b28173fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f548ad69b60a9be2dc5e5adc041467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc8e3f5ec537eca8268",
+    "url": "/static/js/2.83371f29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.83371f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e25492bc97d7198c050",
+    "url": "/static/js/main.6ed506d2.chunk.js"
+  },
+  {
+    "revision": "751c1c6df5f291f0b1f0",
+    "url": "/static/js/runtime-main.a3dcb9f3.js"
+  }
+]);
